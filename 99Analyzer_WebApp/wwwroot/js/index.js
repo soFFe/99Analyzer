8e96873b22bcefb4a4109a4e532d52ce34353f85
soFFe <soffewl@gmail.com>
2019-06-28 06:44:08 +0200

    [WebApp] Map Stats, Error Container, Remove linq.js - added Played, Maps won/lost, Rounds won/lost - fixed the error container sticky position - removed linq.js as I played around during early development and it's unnecessary

--- 99Analyzer_WebApp/wwwroot/js/index.js
+++ 99Analyzer_WebApp/wwwroot/js/index.js
@@ -10,10 +10,16 @@ $.validator.addMethod(
 
 // Cached Data for teams that are displayed
 var rawData = [];
+var filterData = [];
 var chartData = {
     bans: [],
     picks: [],
-    disregarded: []
+    disregarded: [],
+    played: [],
+    mapsWon: [],
+    mapsLost: [],
+    roundsWon: [],
+    roundsLost: []
 };
 var charts = {};
 
@@ -95,23 +101,28 @@ function interpretData(cacheIndex) {
     let data = rawData[cacheIndex],
         bans = [], picks = [],
         enemyBans = [], enemyPicks = [],
-        disregarded = [];
+        disregarded = [], played = [],
+        mapsWon = [], mapsLost = [],
+        roundsWon = {}, roundsLost = {};
 
     // we're tracking all vote data we have - ignoring if the match was actually played or not
     $.each(data.matches.filter(m => m.votes.length > 0), function (i, match) {
         // our teamnumber - not making sure if we're even in the names object.
         // trusts the backend's data integrity..
         let teamNumber = match.teamNames.A == data.team.name ? 0 : 1;
+        let enemyTeamNumber = match.teamNames.A == data.team.name ? 1 : 0;
 
         $.each(match.votes, function (i, vote) {
             if (vote.type == 0) {
+                // ban
                 if (vote.team == teamNumber)
                     bans.push(vote.map);
                 else
                     enemyBans.push(vote.map);
             }
             else if (vote.type == 1) {
-                if(vote.team == teamNumber)
+                // pick
+                if (vote.team == teamNumber)
                     picks.push(vote.map);
                 else
                     enemyPicks.push(vote.map);
@@ -127,79 +138,88 @@ function interpretData(cacheIndex) {
         let disregardedMaps = _MapPool[appliedMapPool].filter(m => votedMaps.indexOf(m) === -1);
 
         disregarded = disregarded.concat(disregardedMaps);
+
+        // get map losses / wins
+        for (var map of match.maps) {
+            let mapScore = Object.values(map.score);
+
+            // maps won/lost
+            if (mapScore[teamNumber] > mapScore[enemyTeamNumber])
+                mapsWon.push(map.name);
+            else
+                mapsLost.push(map.name);
+
+            // rounds won
+            if (Object.keys(roundsWon).indexOf(map.name) === -1)
+                roundsWon[map.name] = mapScore[teamNumber];
+            else
+                roundsWon[map.name] += mapScore[teamNumber]
+
+            // rounds lost
+            if (Object.keys(roundsLost).indexOf(map.name) === -1)
+                roundsLost[map.name] = mapScore[enemyTeamNumber];
+            else
+                roundsLost[map.name] += mapScore[enemyTeamNumber];
+        }
     });
 
+    let tmpPlayed = data.matches.filter(m => m.status == 2).map(m => m.maps);
+    for (var maps of tmpPlayed) {
+        for (var map of maps) {
+            played.push(map.name);
+        }
+    }
+
+    // set data
     chartData.bans[cacheIndex] = bans.map(m => m.name).reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
     chartData.picks[cacheIndex] = picks.map(m => m.name).reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
     chartData.disregarded[cacheIndex] = disregarded.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+    chartData.played[cacheIndex] = played.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+    chartData.mapsWon[cacheIndex] = mapsWon.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+    chartData.mapsLost[cacheIndex] = mapsLost.reduce((a, c) => (a[c] = (a[c] || 0) + 1, a), Object.create(null));
+    chartData.roundsWon[cacheIndex] = roundsWon;
+    chartData.roundsLost[cacheIndex] = roundsLost;
 }
 
 /**
- * Create the charts from the chartsData object
- * Considers the mappool
+ * Creates all map charts from the chartData object
  */
 function createCharts(cacheIndex) {
     if (cacheIndex == -1)
         cacheIndex = rawData.length - 1;
 
+    // (re)initialize charts
+    charts.bans = createMapChart('bans-chart', chartData.bans[cacheIndex]);
+    charts.picks = createMapChart('picks-chart', chartData.picks[cacheIndex]);
+    charts.disregarded = createMapChart('disregarded-chart', chartData.disregarded[cacheIndex]);
+    charts.played = createMapChart('played-chart', chartData.played[cacheIndex]);
+    charts.mapsWon = createMapChart('maps-won-chart', chartData.mapsWon[cacheIndex]);
+    charts.mapsLost = createMapChart('maps-lost-chart', chartData.mapsLost[cacheIndex]);
+    charts.roundsWon = createMapChart('rounds-won-chart', chartData.roundsWon[cacheIndex]);
+    charts.roundsLost = createMapChart('rounds-lost-chart', chartData.roundsLost[cacheIndex]);
+}
+
+/**
+ * Creates a map chart and returns its handle
+ * Expands the label array with the current mappool
+ */
+function createMapChart(element, mapData) {
     let currentMapPool = Object.values(_MapPool);
     currentMapPool = currentMapPool[currentMapPool.length - 1];
 
-    // bans chart
-    let banLabels = Object.keys(chartData.bans[cacheIndex] || {});
+    let mapLabels = Object.keys(mapData || {});
     for (let map of currentMapPool) {
-        if (banLabels.indexOf(map) === -1)
-            banLabels.push(map);
+        if (mapLabels.indexOf(map) === -1)
+            mapLabels.push(map);
     }
-    let ctx = document.getElementById('bans-chart');
-    charts.bans = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: banLabels,
-            datasets: [{
-                data: Object.values(chartData.bans[cacheIndex] || {}),
-                backgroundColor: ["#007bff", "#28a745", "#dc3545", "#ffc107", "#9C27B0", "#FF9800", "#03f2f4", "#795548", "#9e9e9e"],
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-        }
-    });
 
-    // picks chart
-    let pickLabels = Object.keys(chartData.picks[cacheIndex] || {});
-    for (let map of currentMapPool) {
-        if (pickLabels.indexOf(map) === -1)
-            pickLabels.push(map);
-    }
-    ctx = document.getElementById('picks-chart');
-    charts.picks = new Chart(ctx, {
+    ctx = document.getElementById(element);
+    return new Chart(ctx, {
         type: 'pie',
         data: {
-            labels: pickLabels,
+            labels: mapLabels,
             datasets: [{
-                data: Object.values(chartData.picks[cacheIndex] || {}),
-                backgroundColor: ["#007bff", "#28a745", "#dc3545", "#ffc107", "#9C27B0", "#FF9800", "#03f2f4", "#795548", "#9e9e9e"],
-            }]
-        },
-        options: {
-            maintainAspectRatio: false,
-        }
-    });
-
-    // disregarded chart
-    let drLabels = Object.keys(chartData.picks[cacheIndex] || {});
-    for (let map of currentMapPool) {
-        if (drLabels.indexOf(map) === -1)
-            drLabels.push(map);
-    }
-    ctx = document.getElementById('disregarded-chart');
-    charts.picks = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: drLabels,
-            datasets: [{
-                data: Object.values(chartData.disregarded[cacheIndex] || {}),
+                data: Object.values(mapData || {}),
                 backgroundColor: ["#007bff", "#28a745", "#dc3545", "#ffc107", "#9C27B0", "#FF9800", "#03f2f4", "#795548", "#9e9e9e"],
             }]
         },
@@ -214,17 +234,17 @@ function createCharts(cacheIndex) {
  */
 function bindCollapseEvents() {
     $('button.collapse-toggle').off('click');
-    $('button.collapse-toggle').on('click', function(){
+    $('button.collapse-toggle').on('click', function () {
         let $this = $(this),
             $target = $($this.data('target')),
             svgMaximize = feather.icons["maximize-2"],
             svgMinimize = feather.icons["minimize-2"],
             chosenSvg;
 
-        if($target.hasClass("collapsing"))
+        if ($target.hasClass("collapsing"))
             return false;
 
-        if($target.hasClass("show"))
+        if ($target.hasClass("show"))
             chosenSvg = svgMaximize;
         else
             chosenSvg = svgMinimize;
